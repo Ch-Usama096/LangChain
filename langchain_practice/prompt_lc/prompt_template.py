a983ef5d20e706a20042afb104ddff948b2df354
Usama Husnain <usamahusnain096@gmail.com>
2024-11-16 12:26:50 -0600

    Create Prompt Having Not Input Var.

--- langchain_practice/prompt_lc/prompt_template.py
+++ langchain_practice/prompt_lc/prompt_template.py
@@ -2,4 +2,18 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter , RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.prompts import PromptTemplate
 
+# Example No 1: --> Prompt Without Inputs Variables
+
+# method No 1
+promptData1 = PromptTemplate(
+        input_variables = [] , template = "Tell me about Python Language"
+).format()
+
+# Method No 2
+promptData2 = PromptTemplate.from_template("Tell me about Python Language").format()
+
+# Display the Prompt
+print(f"\nHere is the first Method Prompt  : \n{promptData1}\n")
+print(f"Here is the Second Method Prompt : \n{promptData2}\n")
