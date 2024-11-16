b7ff2bf01787b75bf0047727e0550424882f32d3
Usama Husnain <usamahusnain096@gmail.com>
2024-11-16 12:11:46 -0600

    Doing Prompt Related Work

--- langchain_practice/prompt_lc/prompt_template.py
+++ langchain_practice/prompt_lc/prompt_template.py
@@ -1,1 +1,5 @@
 # Import the All Important Modules
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter , RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+
