e0b6d26503a3a58f30738721b0ac746a0065c552
Usama Husnain <usamahusnain096@gmail.com>
2024-11-17 14:05:54 -0600

    FAISS Retriever DB

--- langchain_practice/vector_store/FAISS.py
+++ langchain_practice/vector_store/FAISS.py
@@ -1,1 +1,22 @@
 # Import the Important Modules 
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+# Define the Model for the Embeding
+MODEL_NAME = "all-MiniLM-L6-v2"
+embedding  = HuggingFaceEmbeddings(model_name = MODEL_NAME)
+
+# Define the document
+doc = ["My Name is Usama" , "This is FAISS" , "Hello, How are you" , "My Age is 24" , "FAISS is a langchain module"]
+
+# Define the Vector 
+vector = FAISS.from_texts(doc , embedding)
+retriever = vector.as_retriever(search_type = "similarity" , k = 1)
+
+# Now Fetch the Data from the Retriever Vector DataBase
+query = "what is FAISS"
+answer = retriever.invoke(query)
+
+# Display the Answer
+print(answer)
